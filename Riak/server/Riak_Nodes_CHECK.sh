17e5f4daf66f4c842fe5e242b83378ada3b634d7
Alex Garland <alex.d.garland@gmail.com>
2014-12-30 17:18:03 +0000

    Riak checking via script

---
+++ Riak/server/Riak_Nodes_CHECK.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+RIAKDEVPATH=~/riak-2.0.2/dev/
+
+$RIAKDEVPATH/dev1/bin/riak-admin member-status
+
--- Riak/server/Riak_Nodes_JOIN.sh
+++ Riak/server/Riak_Nodes_JOIN.sh
@@ -6,3 +6,10 @@ do
 	$RIAKDEVPATH/dev$n/bin/riak-admin cluster join dev1@127.0.0.1;
 done
 
+$RIAKDEVPATH/dev1/bin/riak-admin cluster plan
+$RIAKDEVPATH/dev1/bin/riak-admin cluster commit
+
+printf "\n\nCHECKING STATUS:\n\n"
+
+$RIAKDEVPATH/dev1/bin/riak-admin member-status
+
