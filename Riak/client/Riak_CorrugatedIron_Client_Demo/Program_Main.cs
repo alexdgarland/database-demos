3409ed2dc715ef87dd611f651c28fe98b8b94e87
Alex Garland <alex.d.garland@gmail.com>
2014-12-31 08:24:03 +0000

    First basic working version of Riak client code.

---
+++ Riak/client/Riak_CorrugatedIron_Client_Demo/Program_Main.cs
@@ -0,0 +1,28 @@
+﻿using CorrugatedIron;
+using System;
+
+namespace Riak_CorrugatedIron_Client_Demo
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IRiakClient client = Connect();
+                Console.WriteLine("Getting message...");
+                Console.WriteLine(GetDemoMessage(client));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error occured: " + e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+    }
+
+}
---
+++ Riak/client/Riak_CorrugatedIron_Client_Demo/Requests.cs
@@ -0,0 +1,17 @@
+﻿using CorrugatedIron;
+using System;
+using System.Text;
+
+namespace Riak_CorrugatedIron_Client_Demo
+{
+    partial class Program
+    {
+
+        static String GetDemoMessage(IRiakClient client)
+        {
+            var result = client.Get(bucket: "demo", key: "message");
+            return Encoding.Default.GetString(result.Value.Value);
+        }
+
+    }
+}
