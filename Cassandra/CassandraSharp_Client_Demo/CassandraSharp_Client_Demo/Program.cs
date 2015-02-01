1bcc07c52a14a8f9cbf8b96ad8521934d863ecb7
Alex Garland <alex.d.garland@gmail.com>
2015-02-01 21:55:05 +0000

    Add a couple of shell solutions ready to start testing Cassandra clients

---
+++ Cassandra/CassandraSharp_Client_Demo/CassandraSharp_Client_Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CassandraSharp;
+
+namespace CassandraSharp_Client_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
