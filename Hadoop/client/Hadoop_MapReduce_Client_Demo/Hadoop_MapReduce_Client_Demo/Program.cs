896c3a3b48187a6966c4497513ae10399484d71d
Alex Garland <alex.d.garland@gmail.com>
2015-02-08 20:04:41 +0000

    Start setting things up for Neo4j and Hadoop demos.

---
+++ Hadoop/client/Hadoop_MapReduce_Client_Demo/Hadoop_MapReduce_Client_Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Hadoop;
+using Microsoft.Hadoop.MapReduce;
+using Microsoft.Hadoop.WebClient.WebHCatClient;
+
+// Start with this - http://blogs.msdn.com/b/data_otaku/archive/2013/09/07/hadoop-for-net-developers-implementing-a-simple-mapreduce-job.aspx
+// - and see how we get on ...
+
+namespace Hadoop_MapReduce_Client_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
