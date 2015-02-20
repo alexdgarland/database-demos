aef03fab43b1566661877567215d34a628fd7280
Alex Garland <alex.d.garland@gmail.com>
2015-02-20 23:46:34 +0000

    Hadoop - extract common bash code around MR jobs into single file

---
+++ Hadoop/Java_MapReduce/Bash/MapReduceHelperFunctions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function HandleAction
+{
+
+    local action=$1
+    local callingscriptdir=$2
+    local mainclass=$3
+    local dfsoutfolder=$4
+
+    local dfsoutpath=/user/$(whoami)/output/$dfsoutfolder
+
+    if [ $action = "run" ] ; then
+
+        dfsinpath=/user/$(whoami)/input
+        
+        echo "*** Deleting output directory (if exists) ***"
+        hadoop fs -rm -r $dfsoutpath/
+        echo "*** Running MapReduce job ***"
+        hadoop jar $callingscriptdir/AddressPartCounts.jar Jobs.$mainclass $dfsinpath/ $dfsoutpath/
+
+    elif [ $action = "view" ] ; then
+
+        hadoop fs -cat $dfsoutpath/part-r-00000
+
+    else
+
+        printf "ERROR: Parameter missing or not recognised (should be \"run\" or \"view\").\n"
+
+    fi
+
+}
+
--- Hadoop/Java_MapReduce/PostalDistrict.sh
+++ Hadoop/Java_MapReduce/PostalDistrict.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
 action=$1
+
 scriptdir=$(dirname "${BASH_SOURCE[0]}")
 
-if [ $action = "run" ] ; then
+source $scriptdir/Bash/MapReduceHelperFunctions.sh
 
-    indir=/user/$(whoami)/input/
-    outdir=/user/$(whoami)/output/postaldistrict/
-
-    echo "*** Deleting output directory (if exists) ***"
-    hadoop fs -rm -r $outdir
-    echo "*** Running MapReduce job ***"
-    hadoop jar $scriptdir/AddressPartCounts.jar Jobs.PostalDistrictCount $indir $outdir
-
-elif [ $action = "view" ] ; then
-
-    hadoop fs -cat /user/$(whoami)/output/postaldistrict/part-r-00000
-
-else
-
-    printf "Parameter missing or not recognised (should be \"run\" or \"view\"\n"
-
-fi
+HandleAction $action $scriptdir PostalDistrictCount postaldistrict
 
