f92d57824839e620aeadabf8529a1c4abb217d0e
Alex Garland <alex.d.garland@gmail.com>
2015-02-28 20:41:35 +0000

    Try & clean up formatting for SQL in browser

--- Hadoop/Java_MapReduce_Intermediate/FilterSplitPostcodes/StaticPartitionAnalysis.sql
+++ Hadoop/Java_MapReduce_Intermediate/FilterSplitPostcodes/StaticPartitionAnalysis.sql
@@ -4,20 +4,20 @@ having already done some cumulative percentage numbers for individual parts.
 */
 
 WITH CTE_Partition AS
-	(
-	SELECT	CASE
-				WHEN Country <> 'England' THEN 'Non-English'
-				WHEN County = '' AND LEFT(District, 1) < 'F' THEN 'English Missing County, Districts 1'
-				WHEN County = '' AND LEFT(District, 1) < 'R' THEN 'English Missing County, Districts 2'
-				WHEN County = '' THEN 'English Missing County, Districts 3'
-				WHEN County < 'L' THEN 'English Counties 1'
-				ELSE 'English Counties 2'
-			END AS PartitionGroup
-	FROM	dbo.Postcodes
-	WHERE	Terminated = ''
-	)
+        (
+        SELECT  CASE
+                    WHEN Country <> 'England' THEN 'Non-English'
+                    WHEN County = '' AND LEFT(District, 1) < 'F' THEN 'English Missing County, Districts 1'
+                    WHEN County = '' AND LEFT(District, 1) < 'R' THEN 'English Missing County, Districts 2'
+                    WHEN County = '' THEN 'English Missing County, Districts 3'
+                    WHEN County < 'L' THEN 'English Counties 1'
+                    ELSE 'English Counties 2'
+                END AS PartitionGroup
+    FROM	dbo.Postcodes
+    WHERE   Terminated = ''
+    )
 SELECT	PartitionGroup, COUNT(*) AS PartitionRowCount
-FROM CTE_Partition
+FROM    CTE_Partition
 GROUP BY PartitionGroup;
 
 /*
