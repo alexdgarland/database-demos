0de5cb4873538b85a62ef060368b0bcf3dc363b9
Alex Garland <alex.d.garland@gmail.com>
2015-04-01 00:04:19 +0100

    DocumentDB: Start setting up demo project.

---
+++ Azure_DocumentDB/DocumentDB_Demo/DocumentDB_Demo/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+using Microsoft.Azure.Documents.Client;
+
+
+namespace DocumentDB_Demo
+{
+    class Client
+    {
+        /*
+        Get method will read a config file stored outside the source control tree,
+        read out Azure connection details and use these to initialise a client.
+
+        XML config file needs to be stored in current user home directory, called "DocumentDB_Demo.config"
+        and structured as:
+         
+        <?xml version="1.0" encoding="utf-8" ?>
+        <connection>
+            <endpointURL>(PUT YOUR AZURE DOCUMENTDB URI HERE)</endpointURL>
+            <authKey>(PUT YOUR AZURE AUTH KEY HERE)</authKey>
+        </connection>
+         
+        */
+        public static DocumentClient Get()
+        {
+            String homePath = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+            
+            var document = XDocument.Load(homePath + "\\DocumentDB_Demo.config");
+            
+            String endpointURL = document.Descendants("endpointURL").Single<XElement>().Value;
+            String authKey = document.Descendants("authKey").Single<XElement>().Value;
+            
+            return new DocumentClient(new Uri(endpointURL), authKey);
+        }
+
+    }
+}
---
+++ Azure_DocumentDB/DocumentDB_Demo/DocumentDB_Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+
+// Demo of DocumentDB / SDK preview - see http://azure.microsoft.com/en-gb/documentation/articles/documentdb-get-started/
+
+
+namespace DocumentDB_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentClient client = Client.Get();
+
+
+
+
+        }
+    }
+}
