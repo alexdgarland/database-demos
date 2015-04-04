3c1352608bc8eac8de5b9ac1298a04414b391b05
Alex Garland <alex.d.garland@gmail.com>
2015-04-04 15:12:49 +0100

    DocumentDB: Working version of demo

---
+++ Azure_DocumentDB/DocumentDB_Demo/DocumentDB_Demo/DemoSetup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+
+namespace DocumentDB_Demo
+{
+    class DemoSetup
+    {
+
+        /*
+        GetClient method will read a config file stored outside the source control tree,
+        read out Azure connection details and use these to initialise a client.
+
+        XML config file needs to be stored in current user home directory, called "DocumentDB_Demo.config"
+        and structured as:
+         
+        <?xml version="1.0" encoding="utf-8" ?>
+        <connection>
+            <endpointURL>(PUT YOUR AZURE DOCUMENTDB URI HERE)</endpointURL>
+            <authKey>(PUT YOUR AZURE AUTH KEY HERE)</authKey>
+        </connection>
+         
+        */
+        public static DocumentClient GetClient()
+        {
+            String homePath = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+            
+            var document = XDocument.Load(homePath + "\\DocumentDB_Demo.config");
+            
+            String endpointURL = document.Descendants("endpointURL").Single<XElement>().Value;
+            String authKey = document.Descendants("authKey").Single<XElement>().Value;
+            
+            return new DocumentClient(new Uri(endpointURL), authKey);
+        }
+
+
+        /*
+        Simple method to get a database connection, creating the database if it doesn't already exist.
+        */
+        public static Database GetDatabase(DocumentClient client, String databaseId)
+        {
+            var database = client.CreateDatabaseQuery()
+                .Where(db => db.Id == databaseId)
+                .AsEnumerable<Database>()
+                .FirstOrDefault();
+            
+            if (database == null)
+            {
+                database = client.CreateDatabaseAsync(new Database { Id = databaseId }).Result;
+            }
+            
+            return database;
+        }
+
+        public static DocumentCollection GetCollection(DocumentClient client, Database database, String collectionId)
+        {
+            var documentCollection = client.CreateDocumentCollectionQuery(database.CollectionsLink)
+                .Where(c => c.Id == collectionId)
+                .AsEnumerable<DocumentCollection>()
+                .FirstOrDefault();
+
+            if (documentCollection == null)
+            {
+                documentCollection = client.CreateDocumentCollectionAsync
+                    (
+                    database.CollectionsLink,
+                    new DocumentCollection { Id = collectionId }
+                    ).Result;
+            }
+
+            return documentCollection;
+        }
+
+    }
+}
--- Azure_DocumentDB/DocumentDB_Demo/DocumentDB_Demo/Program.cs
+++ Azure_DocumentDB/DocumentDB_Demo/DocumentDB_Demo/Program.cs
@@ -8,20 +8,93 @@ using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.Documents.Linq;
 
+
 // Demo of DocumentDB / SDK preview - see http://azure.microsoft.com/en-gb/documentation/articles/documentdb-get-started/
+// Main example code from Microsoft is available here - https://github.com/Azure/azure-documentdb-net/tree/master/tutorials/get-started
+// and uses a lot of the async/ await features of the SDK; I have removed some of this for maximum simplicity.
 
 
 namespace DocumentDB_Demo
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            DocumentClient client = Client.Get();
+
+            // Setup Azure connection -
+            // some detail is hidden by static methods specific to this demo
+            DocumentClient client = DemoSetup.GetClient();
+            Database database = DemoSetup.GetDatabase(client, "CustomerDB");
+            DocumentCollection collection = DemoSetup.GetCollection(client, database, "CustomerCollection");
 
 
+            // Create a customer in memory
 
+            Customer c1 = Customer.GetDefault();
+            Console.WriteLine(String.Format("\nInitial Customer:\n\n{0}\n", c1.ToString()));
+            /* Keep a record of the customer's ID and name for use in demo queries */
+            int savedCustomerId = c1.CustomerID;
+            String savedCustomerName = c1.Name; 
+
+
+            // Save the customer to DocumentDB.
+            // Note that unlike (e.g.) MongoDB, Riak, creating a document is not idempotent (PUT semantics).
+            // If a document may exist already, we need to check for it and delete/ update.
+            // This extra level of safety/ overhead mat or may not be desirable.
+
+            Document document = client.CreateDocumentQuery(collection.DocumentsLink)
+                                    .Where(d => d.Id == savedCustomerId.ToString())
+                                    .AsEnumerable()
+                                    .SingleOrDefault();
+            if (document != null)
+            {
+                client.DeleteDocumentAsync(document.SelfLink);
+            }
+
+            client.CreateDocumentAsync
+                (
+                collection.DocumentsLink
+                , c1
+                , disableAutomaticIdGeneration : true    /* Control our own document IDs; DocumentDB can also auto-create. */
+                ); ;
+
+
+            // Retrieve the customer details into a new customer object using SQL query (on Document ID/ key)
+
+            String sqlQuery = String.Format("SELECT * FROM Customers c WHERE c.id = '{0}'", savedCustomerId);
+            Console.WriteLine("\nQuerying for customer using SQL string \"" + sqlQuery + "\"");
+
+            Customer c2 = client.CreateDocumentQuery<Customer>(collection.DocumentsLink, sqlQuery)
+                            .AsEnumerable()
+                            .SingleOrDefault();
+            Console.WriteLine(String.Format("\nCustomer retrieved using SQL:\n\n{0}\n", c2.ToString()));
+
+
+            // Retrieve the customer details into a new customer object using LINQ (on Document ID/ key)
+
+            Customer c3 = client.CreateDocumentQuery<Customer>(collection.DocumentsLink)
+                            .Where(c => c.id == savedCustomerId.ToString())
+                            .AsEnumerable()
+                            .FirstOrDefault();
+
+            Console.WriteLine(String.Format("\nCustomer retrieved using LINQ:\n\n{0}\n", c3.ToString()));
+
+
+            // Query on something other than the key field -
+            // would advise some awareness of indexing/ performance implications!
+            // This uses LINQ, when indexing is on same can be done with SQL queries.
+
+            Customer c4 = client.CreateDocumentQuery<Customer>(collection.DocumentsLink)
+                            .Where(c => c.Name == savedCustomerName)
+                            .AsEnumerable()
+                            .FirstOrDefault();
+
+            Console.WriteLine(String.Format("\nCustomer retrieved using LINQ against non-key field (Name):\n\n{0}\n", c4.ToString()));
 
         }
+
     }
 }
+
+
