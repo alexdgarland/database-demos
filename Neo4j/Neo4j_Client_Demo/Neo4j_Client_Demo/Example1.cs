2019102138a42d382bc1914f8a4d495342a8ed05
Alex Garland <alex.d.garland@gmail.com>
2015-04-22 23:26:51 +0100

    Neo4j: Remove data access code from derived Customer class

--- Neo4j/Neo4j_Client_Demo/Neo4j_Client_Demo/Example1.cs
+++ Neo4j/Neo4j_Client_Demo/Neo4j_Client_Demo/Example1.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 
 using Neo4jClient;
 
+using Shared.Customer;
 
 namespace Neo4j_Client_Demo
 {
     class Example1
     {
+
 
         public static void Run()
         {
@@ -23,13 +25,46 @@ namespace Neo4j_Client_Demo
 
             // Save them to Neo4j
 
-            foreach (var c in new List<Neo4jCustomer> { Customer1, Customer2, Customer3 })
+            foreach (var customer in new List<Neo4jCustomer> { Customer1, Customer2, Customer3 })
             {
-                Console.WriteLine("Saving customer to graph:\n{0}\n", c.ToString());
-                c.SaveToGraph(client);
+                Console.WriteLine("Saving customer to graph:\n{0}\n", customer.ToString());
+                SaveCustomerToGraph(customer, client);
                 Console.WriteLine("\nDONE\n");
             }
         }
+
+
+        private static void SaveCustomerToGraph(Neo4jCustomer customer, GraphClient client)
+        {
+            // Save customer details to Neo4j.
+            // We could combine some of these Cypher queries for efficiency, but let's keep things simple for demo purposes:
+
+            // Merge in the associated store so we know it exists once and only once in the graph
+            client.Cypher
+                .Merge("(store:Store { Name: { StoreName } })")
+                .WithParam("StoreName", customer.NearestStore)
+                .ExecuteWithoutResults();
+
+            // Add the Customer to the graph (merging on CustomerID which we take as unique)
+            client.Cypher
+                .Merge("(customer:Customer { CustomerID: { customerIDParam }})")
+                .OnCreate()
+                .Set("customer = { customerParam }")
+                .WithParams(new { customerIDParam = customer.CustomerID, customerParam = customer })
+                .ExecuteWithoutResults();
+
+            // Create the relationship (again - merge as unique)
+            client.Cypher
+                .Match("(customer:Customer { CustomerID : { customerIDParam } } )", "(store:Store { Name : { storeNameParam } } )")
+                .WithParam("customerIDParam", customer.CustomerID)
+                .WithParam("storeNameParam", customer.NearestStore)
+                .CreateUnique("customer-[:NEAREST_STORE_IS]->store")
+                .ExecuteWithoutResults();
+            // In a real system, if updating customers we would also want to delete possible outdated links to a "NearestStore"
+            // if this link might have changed - but can leave that out for now.
+
+        }
+
 
     }
 }
--- Neo4j/Neo4j_Client_Demo/Neo4j_Client_Demo/Neo4jCustomer.cs
+++ Neo4j/Neo4j_Client_Demo/Neo4j_Client_Demo/Neo4jCustomer.cs
@@ -3,8 +3,6 @@ using System;
 
 using Shared.Customer;
 
-using Neo4jClient;
-
 
 namespace Neo4j_Client_Demo
 {
@@ -19,37 +17,6 @@ namespace Neo4j_Client_Demo
         }
 
         public Neo4jCustomer() { }      // MUST have a default constructor to use for deserialisation from Neo4j
-
-        public void SaveToGraph(GraphClient client)
-        {
-            // Save customer details to Neo4j.
-            // We could combine some of these Cypher queries for efficiency, but let's keep things simple for demo purposes:
-
-            // Merge in the associated store so we know it exists once and only once in the graph
-            client.Cypher
-                .Merge("(store:Store { Name: { StoreName } })")
-                .WithParam("StoreName", this.NearestStore)
-                .ExecuteWithoutResults();
-
-            // Add the Customer to the graph (merging on CustomerID which we take as unique)
-            client.Cypher
-                .Merge("(customer:Customer { CustomerID: { customerIDParam }})")
-                .OnCreate()
-                .Set("customer = { customerParam }")
-                .WithParams(new { customerIDParam = this.CustomerID, customerParam = this })
-                .ExecuteWithoutResults();
-
-            // Create the relationship (again - merge as unique)
-            client.Cypher
-                .Match("(customer:Customer { CustomerID : { customerIDParam } } )", "(store:Store { Name : { storeNameParam } } )")
-                .WithParam("customerIDParam", this.CustomerID)
-                .WithParam("storeNameParam", this.NearestStore)
-                .CreateUnique("customer-[:NEAREST_STORE_IS]->store")
-                .ExecuteWithoutResults();
-            // In a real system, if updating customers we would also want to delete possible outdated links to a "NearestStore"
-            // if this link might have changed - but can leave that out for now.
-
-        }
 
     }
 }
