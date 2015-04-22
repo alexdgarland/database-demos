46559b6411bebea94619da3f9e328d4291c60064
Alex Garland <alex.d.garland@gmail.com>
2015-04-22 22:36:17 +0100

    Serialization: Clean-up/ refactoring

--- Serialization/SerializationDemo/SerializationDemo/JSON_Postgres_Example.cs
+++ Serialization/SerializationDemo/SerializationDemo/JSON_Postgres_Example.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-using Shared.Customer;
-
 // http://www.newtonsoft.com/json/help/html/SerializingJSON.htm , "Install-Package Newtonsoft.Json" - popular option but there are others ...
 using Newtonsoft.Json;
 
-using Npgsql;       // Postgres driver for .NET
+using Npgsql;       // Postgres driver for .NET - API is very close to that of System.Data.SqlClient
 using NpgsqlTypes;
 
+using Shared.Customer;
 
 namespace SerializationDemo
 {
@@ -21,63 +20,65 @@ namespace SerializationDemo
             int savedCustomerId = c1.CustomerID;
 
             String json = JsonConvert.SerializeObject(c1, Formatting.Indented);
-            Console.WriteLine("Serialized to JSON:\n\n{0}\n\n", json);
-
-            
-
-            String retrievedJson;   // Need this outside disposable block scope
+            Console.WriteLine("Serializes to JSON as:\n\n{0}\n\n", json);
 
             using (var conn = DemoSetup.GetPostgresConnection())
             {
                 conn.Open();
 
                 Console.WriteLine("Saving to Postgres ...");
-                
-                using   (var command = new NpgsqlCommand
-                                    (
-                                    // Postgres doesn't have a MERGE/ upsert statement so we have to do something like this:
-                                    @"
-                                    -- Update if already exists
-                                    UPDATE  public.customer
-                                    SET     object = :objectparam
-                                    WHERE   customer_id = :idparam;
-                                    -- Insert if does not exist
-                                    INSERT INTO public.customer (customer_id, object)
-                                    SELECT  :idparam, :objectparam
-                                    WHERE NOT EXISTS (SELECT 1 FROM public.customer WHERE customer_id = :idparam);
-                                    "
-                                    ,conn
-                                    )
-                        )
-                {
-                    command.Parameters.Add(new NpgsqlParameter("idparam", NpgsqlDbType.Integer));
-                    command.Parameters[0].Value = c1.CustomerID;
-                    command.Parameters.Add(new NpgsqlParameter("objectparam", NpgsqlDbType.Json));
-                    command.Parameters[1].Value = json;
-                    command.ExecuteNonQuery();
-                }
-
                 Console.WriteLine("Done\n");
 
-
                 Console.WriteLine("Retrieving copy of customer from Postgres ...");
+                Customer c2 = RetrieveCustomer(savedCustomerId, conn);
+                Console.WriteLine("Done\n");
 
-                using (var command = new NpgsqlCommand("SELECT object FROM public.customer WHERE customer_id = :idparam;", conn))
-                {
-                    command.Parameters.Add(new NpgsqlParameter("idparam", NpgsqlDbType.Integer));
-                    command.Parameters[0].Value = savedCustomerId;
-                    retrievedJson = (String)command.ExecuteScalar();
-                }
-
-            }   // </postgres connection>
-
-            Customer c2 = JsonConvert.DeserializeObject<Customer>(retrievedJson);
-
-            Console.WriteLine("Done\n");
-
-            Console.WriteLine("Retrieved customer:\n\n{0}\n\n", c2);
-
+                Console.WriteLine("Retrieved customer:\n\n{0}\n\n", c2);
+            }
         }
+
+
+        private static void SaveCustomer(Customer customer, NpgsqlConnection conn)
+        {
+            String customerJSON = JsonConvert.SerializeObject(customer, Formatting.Indented);
+
+            using (var command = new NpgsqlCommand())
+            {
+                command.Connection = conn;
+                
+                // Postgres doesn't have a MERGE/ upsert statement so we need a two-part command something like this:
+                command.CommandText = 
+                        @"UPDATE  public.customer SET object = :objectparam WHERE customer_id = :idparam;
+                        
+                        INSERT INTO public.customer (customer_id, object)
+                        SELECT  :idparam, :objectparam
+                        WHERE NOT EXISTS (SELECT 1 FROM public.customer WHERE customer_id = :idparam);";
+                
+                command.Parameters.AddWithValue("idparam", customer.CustomerID);
+                command.Parameters.AddWithValue("objectparam", customerJSON);
+                                
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+        private static Customer RetrieveCustomer(int customerId, NpgsqlConnection conn)
+        {
+            using (var command = new NpgsqlCommand())
+            {
+                command.Connection = conn;
+                
+                command.CommandText = "SELECT object FROM public.customer WHERE customer_id = :idparam;";
+                
+                command.Parameters.Add(new NpgsqlParameter("idparam", NpgsqlDbType.Integer));
+                command.Parameters[0].Value = customerId;
+                
+                String retrievedJson = (String)command.ExecuteScalar();
+                
+                return JsonConvert.DeserializeObject<Customer>(retrievedJson);
+            }
+        }
+
 
     }
 }
--- Serialization/SerializationDemo/SerializationDemo/XML_SQLServer_Example.cs
+++ Serialization/SerializationDemo/SerializationDemo/XML_SQLServer_Example.cs
@@ -11,64 +11,86 @@ namespace SerializationDemo
     public class XML_SQLServer_Example
     {
 
+
         public static void Run()
         {
             Customer c1 = Customer.GetDefault();
             Console.WriteLine("Created new customer:\n\n{0}\n\n", c1);
             int savedCustomerID = c1.CustomerID;
 
-            var serializer = new XmlSerializer(c1.GetType());
-            var writer = new StringWriter();
-            serializer.Serialize(writer, c1);
-            var xml = writer.ToString();
-            Console.WriteLine("Serialized to XML:\n\n{0}\n\n", xml);
-
-            String retrievedXML;
+            Console.WriteLine("Serializes to XML as:\n\n{0}\n\n", SerializeToXMLString(c1));
 
             using (var conn = DemoSetup.GetMSSQLConnection())
             {
                 conn.Open();
 
                 Console.WriteLine("Saving to SQL Server ...");
-
-                using (var command = new SqlCommand())
-                    {
-                        command.Connection = conn;
-                        command.CommandText =   @"
-                                                MERGE INTO dbo.Customer AS tgt
-                                                USING (SELECT  @CustomerID AS CustomerID, @XML AS [Object]) AS src
-                                                    ON src.CustomerID = tgt.CustomerID
-                                                WHEN MATCHED THEN UPDATE
-                                                    SET tgt.[Object] = src.[Object]
-                                                WHEN NOT MATCHED BY TARGET THEN INSERT (CustomerID, Object)
-                                                    VALUES (@CustomerID, @XML);
-                                                ";
-                        command.Parameters.AddWithValue("@CustomerID", c1.CustomerID);
-                        command.Parameters.AddWithValue("@XML", xml);
-                        command.ExecuteNonQuery();
-                    }
-
+                SaveCustomer(c1, conn);
                 Console.WriteLine("Done\n");
-
 
                 Console.WriteLine("Retrieving copy of customer from SQL Server ...");
-
-                using (var command = new SqlCommand())
-                {
-                    command.Connection = conn;
-                    command.CommandText = "SELECT [Object] FROM dbo.Customer WHERE CustomerID = @CustomerID;";
-                    command.Parameters.AddWithValue("@CustomerID", savedCustomerID);
-                    retrievedXML = (String)command.ExecuteScalar();
-                }
-
+                Customer c2 = RetrieveCustomer(savedCustomerID, conn);
                 Console.WriteLine("Done\n");
-
-            }   // </sql connection>
-
-            var c2 = serializer.Deserialize(new StringReader(xml));
-            Console.WriteLine("Retrieved Customer:\n\n{0}\n\n", c2);
-
+                
+                Console.WriteLine("Retrieved Customer:\n\n{0}\n\n", c2);
+            }
         }
+
+
+        static String SerializeToXMLString(object Object)
+        {
+            var serializer = new XmlSerializer(Object.GetType());
+            var writer = new StringWriter();
+            serializer.Serialize(writer, Object);
+            return writer.ToString();
+        }
+
+
+        static T DeserializeFromXMLString<T>(String xml)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            return (T)serializer.Deserialize(new StringReader(xml));
+        }
+
+
+        private static void SaveCustomer(Customer customer, SqlConnection conn)
+        {
+            using (var command = new SqlCommand())
+            {
+                String xml = SerializeToXMLString(customer);
+                
+                command.Connection = conn;
+
+                command.CommandText =
+                    @"MERGE INTO dbo.Customer AS tgt
+                    USING (SELECT @CustomerID AS CustomerID, @XML AS [Object]) AS src
+                        ON src.CustomerID = tgt.CustomerID
+                    WHEN MATCHED THEN UPDATE SET tgt.[Object] = src.[Object]
+                    WHEN NOT MATCHED BY TARGET THEN INSERT (CustomerID, Object) VALUES (@CustomerID, @XML);";
+                
+                command.Parameters.AddWithValue("@CustomerID", customer.CustomerID);
+                command.Parameters.AddWithValue("@XML", xml);
+                
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+        private static Customer RetrieveCustomer(int customerID, SqlConnection conn)
+        {
+            using (var command = new SqlCommand())
+            {
+                command.Connection = conn;
+                command.CommandText = "SELECT [Object] FROM dbo.Customer WHERE CustomerID = @CustomerID;";
+                command.Parameters.AddWithValue("@CustomerID", customerID);
+                
+                String retrievedXML = (String)command.ExecuteScalar();
+
+                return DeserializeFromXMLString<Customer>(retrievedXML);
+            }
+            
+        }
+
 
     }
 
